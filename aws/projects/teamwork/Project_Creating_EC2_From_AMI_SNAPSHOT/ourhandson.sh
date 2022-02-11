fcefc4827005d8b7171864267b77c37c9b075ff0
osmansari <sariosmann74@gmail.com>
2022-02-11 16:54:30 +0300

    Project_Creating_EC2_From_AMI_SNAPSHOT added

---
+++ aws/projects/teamwork/Project_Creating_EC2_From_AMI_SNAPSHOT/ourhandson.sh
@@ -0,0 +1,48 @@
+HomeWork_Hands-On
+-----------------------
+
+
+
+-Creating an instance
+
+Creat an instance name to EC2_B
+During Step 4: Add Storage Add New Volume Size 12 GiB (Note; Device column name "/dev/sdb)
+Connect to EC2_B with SSH consol
+Add a file name nightfigher.txt to the Additional Volume
+
+
+
+-Taking a SnapShot to EC2_A with 2 Volume
+
+
+
+-Creating an AMI with SnapShot of EC2_A
+
+
+
+
+-Creating an EC2 name EB2_B from from AMI of EB2_A
+
+
+
+-Creating an Image from the Snapshot of the Nginx Server and Launching a new Instance
+
+
+
+
+
+
+
+
+
+- At the end of the this hands-on training, students will be able to;
+
+- take snapshots of EC2 instances on AWS console.
+
+- create images from EC2 instances on AWS console.
+
+
+
+
+
+Add a file name nightfigher.txt to the Additional Volume
